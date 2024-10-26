942bbc9b68b10a7d30ae961a847edf850f49e617
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2024-10-26 13:24:06 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def teste():
+    print(request.form);
+    return Response(f"Recived: ", status=200, mimetype="text/plain")
+
+@app.route('/', methods=['GET'])
+def teste2():
+    return Response(f"Lorem Ipsum Dolor Sit Amed ", status=200, mimetype="text/plain")
+
+app.run()
